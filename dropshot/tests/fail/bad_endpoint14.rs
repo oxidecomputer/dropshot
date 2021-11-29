123d0e585fc1440e5ee46680dfedeaa75cbe5ac9
Adam Leventhal <ahl@oxide.computer>
2021-11-29 10:27:24 -0800

    we are inadvertently generating OpenAPI documents with wildcard path parameters (#197)

---
+++ dropshot/tests/fail/bad_endpoint14.rs
@@ -0,0 +1,30 @@
+// Copyright 2021 Oxide Computer Company
+
+#![allow(unused_imports)]
+
+use dropshot::endpoint;
+use dropshot::HttpError;
+use dropshot::HttpResponseOk;
+use dropshot::Path;
+use dropshot::RequestContext;
+use schemars::JsonSchema;
+use serde::Deserialize;
+use std::sync::Arc;
+
+#[derive(JsonSchema, Deserialize)]
+struct PathParams {
+    stuff: Vec<String>,
+}
+
+#[endpoint {
+    method = GET,
+    path = "/assets/{stuff:.*}",
+}]
+async fn must_be_unpublished(
+    _: Arc<RequestContext<()>>,
+    _: Path<PathParams>,
+) -> Result<HttpResponseOk<String>, HttpError> {
+    panic!()
+}
+
+fn main() {}
