c959910769ddbbde964060bbf5ac169f2fa5ba3d
Adam Leventhal <ahl@oxide.computer>
2022-04-08 17:47:45 -0700

    allow consumers to respond with freeform bodies and a particular status code (#310)

--- dropshot/tests/test_openapi.rs
+++ dropshot/tests/test_openapi.rs
@@ -1,7 +1,7 @@
 // Copyright 2022 Oxide Computer Company
 
 use dropshot::{
-    endpoint, ApiDescription, HttpError, HttpResponseAccepted,
+    endpoint, ApiDescription, FreeformBody, HttpError, HttpResponseAccepted,
     HttpResponseCreated, HttpResponseDeleted, HttpResponseHeaders,
     HttpResponseOk, HttpResponseUpdatedNoContent, PaginationParams, Path,
     Query, RequestContext, ResultsPage, TagConfig, TagDetails, TypedBody,
@@ -337,6 +337,18 @@ async fn handler17(
     unimplemented!();
 }
 
+#[endpoint {
+    method = GET,
+    path = "/playing/a/bit/nicer",
+    tags = ["it"],
+}]
+async fn handler18(
+    _rqctx: Arc<RequestContext<()>>,
+) -> Result<HttpResponseOk<FreeformBody>, HttpError> {
+    let (_, body) = Body::channel();
+    Ok(HttpResponseOk(body.into()))
+}
+
 fn make_api(
     maybe_tag_config: Option<TagConfig>,
 ) -> Result<ApiDescription<()>, String> {
@@ -363,6 +375,7 @@ fn make_api(
     api.register(handler15)?;
     api.register(handler16)?;
     api.register(handler17)?;
+    api.register(handler18)?;
     Ok(api)
 }
 
