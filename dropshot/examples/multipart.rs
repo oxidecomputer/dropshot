f4721883e689a30f2d1f2f6b4f0ddea2cbca34f9
Jess Frazelle <github@jessfraz.com>
2023-06-30 11:33:53 -0700

    add test
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

---
+++ dropshot/examples/multipart.rs
@@ -0,0 +1,82 @@
+// Copyright 2023 Oxide Computer Company
+//! Example use of Dropshot for multipart form-data.
+
+use dropshot::endpoint;
+use dropshot::ApiDescription;
+use dropshot::ConfigDropshot;
+use dropshot::ConfigLogging;
+use dropshot::ConfigLoggingLevel;
+use dropshot::HttpError;
+use dropshot::HttpServerStarter;
+use dropshot::MultipartBody;
+use dropshot::RequestContext;
+use http::{Response, StatusCode};
+use hyper::Body;
+
+#[tokio::main]
+async fn main() -> Result<(), String> {
+    // We must specify a configuration with a bind address.  We'll use 127.0.0.1
+    // since it's available and won't expose this server outside the host.  We
+    // request port 0, which allows the operating system to pick any available
+    // port.
+    let config_dropshot: ConfigDropshot = Default::default();
+
+    // For simplicity, we'll configure an "info"-level logger that writes to
+    // stderr assuming that it's a terminal.
+    let config_logging =
+        ConfigLogging::StderrTerminal { level: ConfigLoggingLevel::Info };
+    let log = config_logging
+        .to_logger("example-basic")
+        .map_err(|error| format!("failed to create logger: {}", error))?;
+
+    // Build a description of the API.
+    let mut api = ApiDescription::new();
+    api.register(index).unwrap();
+
+    // Set up the server.
+    let server = HttpServerStarter::new(&config_dropshot, api, (), &log)
+        .map_err(|error| format!("failed to create server: {}", error))?
+        .start();
+
+    // Wait for the server to stop.  Note that there's not any code to shut down
+    // this server, so we should never get past this point.
+    server.await
+}
+
+/// Return static content for all paths.
+#[endpoint {
+    method = POST,
+
+    /*
+     * Match literally every path including the empty path.
+     */
+    path = "/",
+
+    /*
+     * This isn't an API so we don't want this to appear in the OpenAPI
+     * description if we were to generate it.
+     */
+    unpublished = true,
+}]
+async fn index(
+    _rqctx: RequestContext<()>,
+    mut body: MultipartBody,
+) -> Result<Response<Body>, HttpError> {
+    // Iterate over the fields, use `next_field()` to get the next field.
+    while let Some(mut field) = body.content.next_field().await.unwrap() {
+        // Get field name.
+        let name = field.name();
+        // Get the field's filename if provided in "Content-Disposition" header.
+        let file_name = field.file_name();
+
+        println!("Name: {:?}, File Name: {:?}", name, file_name);
+
+        // Process the field data chunks e.g. store them in a file.
+        while let Some(chunk) = field.chunk().await.unwrap() {
+            // Do something with field chunk.
+            println!("Chunk: {:?}", chunk);
+        }
+    }
+
+    Ok(Response::builder().status(StatusCode::OK).body("Ok".into())?)
+}
---
+++ dropshot/tests/test_multipart.rs
@@ -0,0 +1,68 @@
+// Copyright 2023 Oxide Computer Company
+
+//! Test cases for multipart form-data.
+
+use dropshot::test_util::read_string;
+use dropshot::{
+    endpoint, ApiDescription, HttpError, MultipartBody, RequestContext,
+};
+use http::{Method, Response, StatusCode};
+use hyper::Body;
+
+extern crate slog;
+
+pub mod common;
+
+fn api() -> ApiDescription<usize> {
+    let mut api = ApiDescription::new();
+    api.register(api_multipart).unwrap();
+    api
+}
+
+#[endpoint {
+    method = POST,
+    path = "/upload",
+}]
+async fn api_multipart(
+    _rqctx: RequestContext<usize>,
+    mut body: MultipartBody,
+) -> Result<Response<Body>, HttpError> {
+    // Iterate over the fields, use `next_field()` to get the next field.
+    if let Some(field) = body.content.next_field().await.unwrap() {
+        // Process the field data chunks e.g. store them in a file.
+        if let Ok(bytes) = field.bytes().await {
+            return Ok(Response::builder()
+                .status(StatusCode::OK)
+                .body(bytes.into())?);
+        }
+    }
+
+    Err(HttpError::for_internal_error("no field found".to_string()))
+}
+
+#[tokio::test]
+async fn test_multipart_client() {
+    let api = api();
+    let testctx = common::test_setup("multipart_client", api);
+
+    let test_string = "abcd";
+    let mut response = testctx
+        .client_testctx
+        .make_request_with_body(
+            Method::POST,
+            "/upload",
+            format!(
+                "--X-BOUNDARY\r\nContent-Disposition: form-data; \
+        name=\"my_text_field\"\r\n\r\n{}\r\n--X-BOUNDARY--\r\n",
+                test_string
+            )
+            .into(),
+            StatusCode::OK,
+        )
+        .await
+        .expect("expected success");
+    let body = read_string(&mut response).await;
+    assert_eq!(body, test_string);
+
+    testctx.teardown().await;
+}
