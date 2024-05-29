325d1e36dda618ad1245ffcb9f8dad0c02db0df1
Rain <rain@oxide.computer>
2024-05-28 17:40:50 -0700

    Fix test
    Created using spr 1.3.6-beta.1

--- dropshot_endpoint/tests/output/server_basic.rs
+++ dropshot_endpoint/tests/output/server_basic.rs
@@ -6,38 +6,28 @@ pub trait MyTrait: 'static {
         Output = Result<HttpResponseOk<()>, HttpError>,
     > + Send + 'static;
 }
-const _: fn() = || {
-    struct NeedRequestContext(
-        <RequestContext<()> as dropshot::RequestContextArgument>::Context,
-    );
-};
-const _: fn() = || {
-    trait ResultTrait {
-        type T;
-        type E;
-    }
-    impl<TT, EE> ResultTrait for Result<TT, EE>
-    where
-        TT: dropshot::HttpResponse,
+#[automatically_derived]
+pub fn MyTrait_stub_api_description() -> ::std::result::Result<
+    dropshot::ApiDescription<dropshot::StubContext>,
+    dropshot::ApiDescriptionBuildError,
+> {
+    let mut dropshot_api = dropshot::ApiDescription::new();
+    let mut dropshot_errors: Vec<String> = Vec::new();
     {
-        type T = TT;
-        type E = EE;
+        let endpoint_handler_xyz = dropshot::ApiEndpoint::new_stub::<
+            (),
+            Result<HttpResponseOk<()>, HttpError>,
+        >("handler_xyz".to_string(), dropshot::Method::GET, "application/json", "/xyz");
+        if let Err(error) = dropshot_api.register(endpoint_handler_xyz) {
+            dropshot_errors.push(error);
+        }
     }
-    struct NeedHttpResponse(<Result<HttpResponseOk<()>, HttpError> as ResultTrait>::T);
-    trait TypeEq {
-        type This: ?Sized;
+    if !dropshot_errors.is_empty() {
+        Err(dropshot::ApiDescriptionBuildError::new(dropshot_errors))
+    } else {
+        Ok(dropshot_api)
     }
-    impl<T: ?Sized> TypeEq for T {
-        type This = Self;
-    }
-    fn validate_result_error_type<T>()
-    where
-        T: ?Sized + TypeEq<This = dropshot::HttpError>,
-    {}
-    validate_result_error_type::<
-        <Result<HttpResponseOk<()>, HttpError> as ResultTrait>::E,
-    >();
-};
+}
 #[automatically_derived]
 pub fn MyTrait_api_description<ServerImpl: MyTrait>() -> ::std::result::Result<
     dropshot::ApiDescription<<ServerImpl as MyTrait>::Context>,
@@ -53,28 +43,6 @@ pub fn MyTrait_api_description<ServerImpl: MyTrait>() -> ::std::result::Result<
             "application/json",
             "/xyz",
         );
-        if let Err(error) = dropshot_api.register(endpoint_handler_xyz) {
-            dropshot_errors.push(error);
-        }
-    }
-    if !dropshot_errors.is_empty() {
-        Err(dropshot::ApiDescriptionBuildError::new(dropshot_errors))
-    } else {
-        Ok(dropshot_api)
-    }
-}
-#[automatically_derived]
-pub fn MyTrait_stub_api_description() -> ::std::result::Result<
-    dropshot::ApiDescription<dropshot::StubContext>,
-    dropshot::ApiDescriptionBuildError,
-> {
-    let mut dropshot_api = dropshot::ApiDescription::new();
-    let mut dropshot_errors: Vec<String> = Vec::new();
-    {
-        let endpoint_handler_xyz = dropshot::ApiEndpoint::new_stub::<
-            (),
-            Result<HttpResponseOk<()>, HttpError>,
-        >("handler_xyz".to_string(), dropshot::Method::GET, "application/json", "/xyz");
         if let Err(error) = dropshot_api.register(endpoint_handler_xyz) {
             dropshot_errors.push(error);
         }
