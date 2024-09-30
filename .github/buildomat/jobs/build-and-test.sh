b22e9c1343faa44fbbfad532d318cdf9e0ae7692
David Pacheco <dap@oxidecomputer.com>
2024-09-30 16:08:48 -0700

    okay, but the right help

--- .github/buildomat/jobs/build-and-test.sh
+++ .github/buildomat/jobs/build-and-test.sh
@@ -17,4 +17,4 @@ banner build
 ptime -m cargo build --all-features --locked --all-targets --verbose
 
 banner test
-RUST_BACKTRACE=1 ptime -m cargo test --all-features --locked --verbose
+ptime -m cargo test --all-features --locked --verbose
