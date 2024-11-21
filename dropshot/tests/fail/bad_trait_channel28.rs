d5874eb6bc4e93f4ace23f711973f14192710fdb
Rain <rain@oxide.computer>
2024-11-20 19:27:45 -0800

    [5/n] [dropshot] add support for per-endpoint size limits (#1171)
    For some endpoints like uploading update and support bundles, we would like to
    have a larger request limit. Add support for that.
    
    This is the other half of RFD 353, after #617. (A previous attempt was #618,
    which I've abandoned.)
    
    The PR consists of:
    
    * The implementation.
    * A small tweak to the API generation code for better span attribution.
    * Tests.

---
+++ dropshot/tests/fail/bad_trait_channel28.rs
@@ -0,0 +1,44 @@
+// Copyright 2024 Oxide Computer Company
+
+#![allow(unused_imports)]
+
+use dropshot::channel;
+use dropshot::RequestContext;
+use dropshot::WebsocketUpgrade;
+
+#[dropshot::api_description]
+trait MyServer {
+    type Context;
+
+    // Test: request_body_max_bytes specified for channel (this parameter is only
+    // accepted for endpoints, not channels)
+    #[channel {
+        protocol = WEBSOCKETS,
+        path = "/test",
+        request_body_max_bytes = 1024,
+    }]
+    async fn bad_channel(
+        _rqctx: RequestContext<Self::Context>,
+        _upgraded: WebsocketUpgrade,
+    ) -> dropshot::WebsocketChannelResult;
+}
+
+enum MyImpl {}
+
+// This should not produce errors about items being missing.
+impl MyServer for MyImpl {
+    type Context = ();
+
+    async fn bad_channel(
+        _rqctx: RequestContext<Self::Context>,
+        _upgraded: WebsocketUpgrade,
+    ) -> dropshot::WebsocketChannelResult {
+        Ok(())
+    }
+}
+
+fn main() {
+    // These items should be generated and accessible.
+    my_server_mod::api_description::<MyImpl>();
+    my_server_mod::stub_api_description();
+}
